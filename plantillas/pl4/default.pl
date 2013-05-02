2595a17e7fe9b4f8549efb6492a310dbf3b75c6c
amatcan <amatcan@gmail.com>
2013-05-02 17:28:02 +0200

    se

---
+++ plantillas/pl4/default.pl
@@ -0,0 +1,38 @@
+<html>
+<head>
+</head>
+<body>
+<style type="text/css">
+	body{
+		font-size:16px;
+	}
+	.content{
+		background-image: url("{%imagen%}");
+		background-repeat: no-repeat;
+		background-size: 100% auto;
+		height: 100%;
+		width: 100%;
+		text-align:center;
+	}
+	h1{
+		margin-top:0px;
+		padding-top:2em;
+		text-align: center;
+		font-weight:bold;
+		color:white;
+	}
+	h2{
+		color: yellow;
+	}
+	p{
+		color: red;
+		font-weight:bold;
+	}
+</style>
+<div class="content">
+	<h1>{%titulo%}</h1>
+	<h2>{%mensaje%}</h2>
+	<p>{%texto%}</p>
+</div>
+</body>
+</html>
